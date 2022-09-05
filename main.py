3352b65d0889881ec0151a68ef2a64ce31903fda
Egorov Evgeniy <egorovzhenya@gmail.com>
2022-09-06 00:48:00 +0400

    feat: define main scenario

--- main.py
+++ main.py
@@ -1,74 +1,69 @@
 import argparse
 import os
+import logging
 import subprocess
 import pyautogui
 from time import sleep
 from datetime import datetime
-from pathlib import Path
 
-# import psutil
 
+logger = logging.getLogger(__name__)
 
 # processes stdin arguments
-parser = argparse.ArgumentParser(description="Performance measuring")
-parser.add_argument("file_path", help="path to executable file")
-parser.add_argument(
-    "-o",
-    default=os.path.join(os.getcwd(), f"stats_{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"),
-    help="specify a directory to store results",
-)
-args = parser.parse_args()
+def get_args():
+    parser = argparse.ArgumentParser(description="Performance measuring")
+    parser.add_argument("file", help="path to executable file")
+    parser.add_argument(
+        "-o",
+        default=os.path.join(os.getcwd(), f"stats_{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"),
+        help="specify a directory to store results",
+    )
+    args = parser.parse_args()
 
-print(f"file path {args.file_path}")
-print(f"output {args.o}")
+    # checks if the file exists
+    if not os.path.exists(args.file):
+        raise FileNotFoundError("Check if you have passed a correct file path")
 
-# checks if the file exists
-if not os.path.exists(args.file_path):
-    raise FileNotFoundError("Check if you have passed a correct file path")
+    # if the folder is not specified or does not exist, the default directory will be created
+    if not os.path.exists(args.o):
+        os.mkdir(args.o)
 
-# if the path to the output directory is not specified, the default directory will be created
-if not os.path.exists(args.o):
-    os.mkdir(args.o)
+    logging.info(f"Output directory: {args.o}")
 
-# checks if the output directory exists
-# if not os.path.exists(args.o):
-#     raise FileNotFoundError("Check if you have passed a correct output directory path")
+    return args.file, args.o
 
-
-print(f"file path {args.file_path}")
-print(f"output {args.o}")
-
-# def launch_process():
-# launch
-# process = subprocess.Popen([args.file_path, "-f"])
-# sleep(3)
-# check if the process is active
-# if os.path.basename(args.file_path) in (p.name() for p in psutil.process_iter()):
-#     print('WORKING!')
-# return process
+def launch_process():
+    # process = subprocess.Popen([exec_file, "-f"])
+    # check if the proces is launched
+    pass
 
 
-# def press_btn(btn: str, interval=0.0, times=0):
-#     for _ in range(times + 1):
-#         pyautogui.btnDown(btn)
-#         sleep(interval)
-#         pyautogui.btnUp(btn)
-#         sleep(0.5)
+def make_screenshot(dir, name):
+    scr = pyautogui.screenshot()
+    path = os.path.join(dir, name)
+    scr.save(path)
 
 
 def main():
-    process = subprocess.Popen(["runas", "/user:eegor", args.file_path, "-f"])
-    # os.startfile(args.file_path)
-    sleep(15)
-    pyautogui.press("enter", presses=2)
-    prtsc = pyautogui.screenshot()
-    prtsc.save(args.o, "scr_start.png")
-    pyautogui.hold("w")
-    sleep(3)
-    prtsc = pyautogui.screenshot()
-    prtsc.save(args.o, "scr_start.png")
-    process.kill()
+    exec_file, output_dir = get_args()
+
+    #process = launch_process()
+    logging.info("The process is launched")
+
+    make_screenshot(output_dir, 'start_screenshot.png')
+    logging.info("Took screenshot")
+    
+    pyautogui.press('enter', presses=2)
+    pyautogui.keyDown("w")
+    sleep(5)
+    pyautogui.keyUp("w")
+    
+    # process.kill()
+    logging.info("The process is killed")
 
 
-# if __name__=='__main__':
-#     main()
+if __name__=='__main__':
+    logging.basicConfig(
+        format="logger >> %(message)s", level=logging.INFO
+    )
+    main()
