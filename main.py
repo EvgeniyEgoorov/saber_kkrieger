d70e95d86e5b5fa986a44af7f18f484eb43cd318
Egorov Evgeniy <egorovzhenya@gmail.com>
2022-09-07 00:30:16 +0400

    feat: add fraps

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import os
 import logging
 import subprocess
 import pyautogui
+import re
+import csv
+import statistics
 
 from configparser import ConfigParser
+from glob import glob
 from time import sleep
 from datetime import datetime
 
@@ -12,10 +16,10 @@ from datetime import datetime
 logger = logging.getLogger(__name__)
 
 
-# processes stdin arguments
+# processes cmd line arguments
 def get_args():
     parser = argparse.ArgumentParser(description="Performance measuring")
-    parser.add_argument("file", help="path to executable file")
+    parser.add_argument("path", help="path to executable file")
     parser.add_argument(
         "-o",
         default=os.path.join(
@@ -26,7 +30,7 @@ def get_args():
     args = parser.parse_args()
 
     # checks if the file exists
-    if not os.path.exists(args.file):
+    if not os.path.exists(args.path):
         raise FileNotFoundError("Check if you have passed a correct file path")
 
     # if the folder is not specified or does not exist, the default directory will be created
@@ -35,15 +39,33 @@ def get_args():
 
     logging.info(f"Get results here: {args.o}")
 
-    return args.file, args.o
+    return args.path, args.o
 
 
-def launch_process(exec_file):
+def get_config():
+    config = ConfigParser()
+    config.read("config.ini")
+
+    fraps_exe_path = config["fraps"]["exec_file"]
+    fraps_output = config["fraps"]["output"]
+    fraps_hotkey = config["fraps"]["hotkey"]
+
+    return fraps_exe_path, fraps_output, fraps_hotkey
+
+
+def launch_fraps(exec_file):
+    process = subprocess.Popen([exec_file, "-f"])
+    sleep(1)
+    logging.info("Fraps is launched")
+    return process
+
+
+def launch_game(exec_file):
     process = subprocess.Popen([exec_file, "-f"])
     sleep(3)
     while pyautogui.pixelMatchesColor(300, 200, (0, 0, 0)):
         sleep(0.1)
-    logging.info("The process is launched")
+    logging.info("The game is launched")
     return process
 
 
@@ -54,23 +76,48 @@ def press_key(key, time=0.1):
     sleep(1)
 
 
-def make_screenshot(dir, name):
+def make_screenshot(output_folder, file_name):
     scr = pyautogui.screenshot()
-    path = os.path.join(dir, name)
+    path = os.path.join(output_folder, file_name)
     scr.save(path)
-    logging.info(f"{name} was taken")
-    sleep(1)
+    logging.info(f"{file_name} was taken")
+    sleep(0.5)
+
+
+def get_stat(output_folder, fraps_output):
+    fraps_files = glob(fraps_output + "\*.csv")
+    latest_stat = max(fraps_files, key=os.path.getctime)
+
+    read_stat = []
+    with open(latest_stat, "r", newline="") as csvfile:
+        reader = csv.reader(csvfile, delimiter=" ", quotechar="|")
+        for row in reader:
+            try:
+                row = int(row[0])
+            except ValueError:
+                continue
+            read_stat.append(row)
+
+    median_fps = statistics.median(read_stat)
+    with open(os.path.join(output_folder, "median_fps.txt"), "w") as file:
+        file.write(str(median_fps))
+    logging.info("FPS stat is written to median_fps.txt")
 
 
 def kill_process(process):
     process.kill()
     sleep(1)
-    logging.info("The process is killed")
+    logging.info(f"{os.path.basename(process.args[0])} is killed")
 
 
-def run_scenario(exec_file, output_dir):
-    # launch
-    process = launch_process(exec_file)
+def run_scenario(
+    game_exe_path, output_folder, fraps_exe_path, fraps_output, fraps_hotkey
+):
+    # launch fraps
+    process_1 = launch_fraps(fraps_exe_path)
+
+    # launch game
+    process_2 = launch_game(game_exe_path)
 
     # skip intro
     press_key("enter")
@@ -78,17 +125,29 @@ def run_scenario(exec_file, output_dir):
     # start game
     press_key("enter")
 
+    # start measure fps
+    press_key(fraps_hotkey)
+
     # screenshot
-    make_screenshot(output_dir, "start_screenshot.png")
+    make_screenshot(output_folder, "start_screenshot.png")
 
     # move the character forward
     press_key("w", 5)
 
     # screenshot
-    make_screenshot(output_dir, "end_screenshot.png")
+    make_screenshot(output_folder, "end_screenshot.png")
 
-    # terminate execution
-    kill_process(process)
+    # stop measure fps
+    press_key(fraps_hotkey)
+
+    # close game
+    kill_process(process_2)
+
+    # close fraps
+    kill_process(process_1)
+
+    # save stat
+    get_stat(output_folder, fraps_output)
 
 
 if __name__ == "__main__":
@@ -96,5 +155,10 @@ if __name__ == "__main__":
         format="%(asctime)s%(name)5s:%(lineno)3s:%(levelname)s >> %(message)s",
         level=logging.INFO,
     )
-    exec_file, output_dir = get_args()
-    run_scenario(exec_file, output_dir)
+
+    fraps_exe_path, fraps_output, fraps_hotkey = get_config()
+    game_exe_path, output_folder = get_args()
+
+    run_scenario(
+        game_exe_path, output_folder, fraps_exe_path, fraps_output, fraps_hotkey
+    )
